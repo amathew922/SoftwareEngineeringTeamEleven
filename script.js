33485bcbbdb3f8d1d29eb1f891493bc9c26501d6
Ashwin Gopalakrishnan <ashwingopalakrishnan@Ashwins-MBP.attlocal.net>
2024-07-21 11:51:51 -0400

    final changes

--- script.js
+++ script.js
@@ -1,17 +1,46 @@
-let password = document.getElementById("password");
-function genPassword() {
-    var chars = "0123456789abcdefghakshSNUAI2425NYfijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    var passwordLength = 15;
-    var password = "";
-    for (var i = 0; i <= passwordLength; i++) {
-        var randomNumber = Math.floor(Math.random() * chars.length);
-        password += chars.substring(randomNumber, randomNumber + 1);
-    }
-    document.getElementById("password").value = password;
+let passwordField = document.getElementById("password");
 
+function genPassword() {
+    const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+    const numberChars = "0123456789";
+    const specialChars = "!@#$%^&*()";
+    
+    let chars = "";
+    if (document.getElementById("uppercase").checked) {
+        chars += uppercaseChars;
+    }
+    if (document.getElementById("lowercase").checked) {
+        chars += lowercaseChars;
+    }
+    if (document.getElementById("numbers").checked) {
+        chars += numberChars;
+    }
+    if (document.getElementById("special").checked) {
+        chars += specialChars;
+    }
+
+    if (chars === "") {
+        alert("Please select at least one character type.");
+        return;
+    }
+
+    const passwordLength = parseInt(document.getElementById("passwordLength").value);
+    let password = "";
+    for (let i = 0; i < passwordLength; i++) {
+        const randomNumber = Math.floor(Math.random() * chars.length);
+        password += chars.charAt(randomNumber);
+    }
+    passwordField.value = password;
 }
+
 function copyPassword() {
-    var copyText = document.getElementById("password");
-    copyText.select();
-    copyText.setSelectionRange(0, 999);
+    passwordField.select();
+    passwordField.setSelectionRange(0, 99999); // For mobile devices
+    document.execCommand("copy");
+    alert("Password copied to clipboard");
+}
+
+function updateLengthLabel(value) {
+    document.getElementById("lengthLabel").innerText = value;
 }
